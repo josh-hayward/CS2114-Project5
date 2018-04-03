b5062474f54e37169be07cab2f9dc6ee8057e7be
Josh Hayward <35711502+josh-hayward@users.noreply.github.com>
2018-04-02 21:31:49 -0400

    Add files via upload

---
+++ SortType.java
@@ -0,0 +1,18 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with
+// honor and integrity at all times.
+// I will not lie, cheat, or steal, nor
+// will I accept the actions of those who do.
+// -- Joshua Hayward (jhayward)
+
+package prj5;
+
+/**
+ * enum containint the different ways to sort songs
+ * @author Josh Hayward
+ * @version 04.02.2018
+ */
+public enum SortType {
+    ARTIST, GENRE, TITLE, YEAR;
+}

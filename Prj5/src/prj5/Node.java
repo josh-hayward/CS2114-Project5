003cd28182f04ed2f3c3228a90f4695bf62a4aa3
Josh Hayward <35711502+josh-hayward@users.noreply.github.com>
2018-04-04 12:39:32 -0400

    Add files via upload
    changed Node to be its own class instead of a protected inner class

---
+++ Prj5/src/prj5/Node.java
@@ -0,0 +1,68 @@
+package prj5;
+
+/**
+ * This class creates nodes that store some
+ * type of data
+ * 
+ * @author Cameron Moore (cam1111)
+ * @version 2018.04.01
+ * @param T
+ *            A generic type
+ */
+public class Node<T> {
+    
+    private Node<T> next;
+    private T data;
+
+    /**
+     * Initializes next as null
+     * and sets the data within the node
+     * 
+     * @param data
+     *            The data to store in the node
+     */
+    public Node(T data) {
+        next = null;
+        this.data = data;
+    }
+
+
+    /**
+     * Sets the nextNode of a node
+     * 
+     * @param newNode
+     *            The next node in the list
+     */
+    public void setNextNode(Node<T> nextNode) {
+        next = nextNode;
+    }
+
+
+    /**
+     * Sets the data of a node
+     * 
+     * @param data
+     *            The data to be stored in the node
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+
+    /**
+     * Returns the data stored in the node
+     * 
+     * @return data The data in the node
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * Gets the next node
+     * @return next The next node
+     */
+    public Node<T> getNextNode() {
+        return next;
+    }
+}

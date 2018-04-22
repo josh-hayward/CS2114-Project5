539f319092713fbb5db42328b4ed7b2a8a4c3d07
Anthony Farina <farinaa@vt.edu>
2018-04-22 19:48:52 -0400

    Added NodeTest class.

---
+++ Prj5/src/prj5/NodeTest.java
@@ -0,0 +1,77 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Cameron Moore (cam1111)
+// -- Anthony Farina (farinaa)
+// -- Joshua Hayward (jhayward)
+
+package prj5;
+
+import student.TestCase;
+
+/**
+ * This class tests all the methods in the Node class to ensure they all work as
+ * intended.
+ * 
+ * @author Anthony Farina (farinaa)
+ * @version 2018.04.22
+ */
+public class NodeTest extends TestCase {
+
+    /**
+     * Declare the Node object(s) to be tested on.
+     */
+    private Node<String> testNode;
+
+
+    /**
+     * Initialize the Node object(s) for testing.
+     */
+    public void setUp() {
+        testNode = new Node<String>("test");
+    }
+
+
+    /**
+     * Tests the getData method in the Node class to ensure it returns the
+     * correct data of this Node.
+     */
+    public void testGetData() {
+        assertEquals("test", testNode.getData());
+    }
+
+
+    /**
+     * Tests the setData method in the Node class to ensure it changes the data
+     * in this Node to the given data.
+     */
+    public void testSetData() {
+        assertEquals("test", testNode.getData());
+        testNode.setData("new test");
+        assertEquals("new test", testNode.getData());
+    }
+
+
+    /**
+     * Tests the getNextNode method in the Node class to ensure it returns the
+     * correct Node that this Node points to.
+     */
+    public void testGetNextNode() {
+        assertTrue(testNode.getNextNode() == null);
+    }
+
+
+    /**
+     * Tests the setNextNode method in the Node class to ensure it updates the
+     * next Node this Node points to properly.
+     */
+    public void testSetNextNode() {
+        assertTrue(testNode.getNextNode() == null);
+
+        Node<String> next = new Node<String>("next");
+        testNode.setNextNode(next);
+        assertTrue(testNode.getNextNode() == next);
+    }
+}

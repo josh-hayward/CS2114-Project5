a08e48e560cc9bbe3a7659f4ae8a9e74fed20208
Anthony Farina <farinaa@vt.edu>
2018-04-22 20:26:58 -0400

    Updated toString()

--- Prj5/src/prj5/SongList.java
+++ Prj5/src/prj5/SongList.java
@@ -25,88 +25,93 @@ public class SongList extends LinkedList<Song> {
     public SongList() {
         super();
     }
-    
+
+
     /**
      * returns song at given index
+     * 
      * @return song at given index
-     * @param index The index of the song to get
+     * @param index
+     *            The index of the song to get
      */
     public Song getSong(int index) {
         return getNodeAt(index).getData();
     }
 
+
     /**
      * Sorts by artist
      */
-    public void sortArtist()
-    {
+    public void sortArtist() {
         sortBy(SortTypeEnum.ARTIST);
     }
-    
+
+
     /**
      * Sorts by genre
      */
-    public void sortGenre()
-    {
+    public void sortGenre() {
         sortBy(SortTypeEnum.GENRE);
     }
-    
+
+
     /**
      * Sorts by title
      */
-    public void sortTitle()
-    {
+    public void sortTitle() {
         sortBy(SortTypeEnum.TITLE);
     }
-    
+
+
     /**
      * Sorts by year
      */
-    public void sortYear()
-    {
+    public void sortYear() {
         sortBy(SortTypeEnum.YEAR);
     }
-    
-    ///**
-    // * Creates a string representation of
-    // * the titles of each each
-    // * @return String a string of the songs
-    // */
-    //public String toString()
-    //{
-    //    String list = "[";
-    //    Node<Song> curr = getNodeAt(0);
-    //    while (curr != null) {
-    //        list = list + curr.getData().toString();
-    //        if (curr.getNextNode() != null) {
-    //            list = list + ", ";
-    //        }
-    //        curr = curr.getNextNode();
-    //    }
-    //    return list + "]";
-    //}
-    
-    
+
+
+    /**
+     * Creates a string representation of
+     * the titles of each each
+     * 
+     * @return String a string of the songs
+     */
+    public String toString() {
+        StringBuilder list = new StringBuilder("[");
+        
+        Node<Song> curr = getNodeAt(0);
+        while (curr != null) {
+            list.append(curr.getData().toString());
+            if (curr.getNextNode() != null) {
+                list.append(", ");
+            }
+            curr = curr.getNextNode();
+        }
+        return list.append("]").toString();
+    }
+
+
     /**
      * uses a simple insertion sort to sort the list by the given song
      * attribute, defaults to sort by artist
-     * @param sortType The type to sort by, including
-     *                 artist, genre, title, and year
+     * 
+     * @param sortType
+     *            The type to sort by, including
+     *            artist, genre, title, and year
      */
     public void sortBy(SortTypeEnum sortType) {
         for (int i = 1; i < size(); ++i) {
-            
+
             Song currentSong = getNodeAt(i).getData();
-            
+
             int j = i - 1;
-            
-            switch (sortType)
-            {
+
+            switch (sortType) {
                 case ARTIST:
-                    while (j >= 0 && getNodeAt(j).getData().getArtist().
-                        toLowerCase().compareTo(currentSong.getArtist().
-                        toLowerCase()) > 0)
-                    {
+                    while (j >= 0 && getNodeAt(j).getData().getArtist()
+                        .toLowerCase().compareTo(currentSong.getArtist()
+                            .toLowerCase()) > 0) {
                         getNodeAt(j + 1).setData(getNodeAt(j).getData());
                         j = j - 1;
                     }
@@ -114,34 +119,31 @@ public class SongList extends LinkedList<Song> {
                     break;
                 case GENRE:
                     while (j >= 0 && getNodeAt(j).getData().getGenre()
-                        .compareTo(currentSong.getGenre()) > 0)
-                    {
+                        .compareTo(currentSong.getGenre()) > 0) {
                         getNodeAt(j + 1).setData(getNodeAt(j).getData());
                         j = j - 1;
                     }
                     getNodeAt(j + 1).setData(currentSong);
                     break;
                 case TITLE:
-                    while (j >= 0 && getNodeAt(j).getData().getTitle().
-                        toLowerCase().compareTo(currentSong.getTitle().
-                        toLowerCase()) > 0)
-                    {
+                    while (j >= 0 && getNodeAt(j).getData().getTitle()
+                        .toLowerCase().compareTo(currentSong.getTitle()
+                            .toLowerCase()) > 0) {
                         getNodeAt(j + 1).setData(getNodeAt(j).getData());
                         j = j - 1;
                     }
                     getNodeAt(j + 1).setData(currentSong);
                     break;
                 case YEAR:
-                    while (j >= 0 && getNodeAt(j).getData().getYear() 
-                        > currentSong.getYear())
-                    {
+                    while (j >= 0 && getNodeAt(j).getData()
+                        .getYear() > currentSong.getYear()) {
                         getNodeAt(j + 1).setData(getNodeAt(j).getData());
                         j = j - 1;
                     }
                     getNodeAt(j + 1).setData(currentSong);
                     break;
                 default:
-                    sortBy(SortTypeEnum.ARTIST);   
+                    sortBy(SortTypeEnum.ARTIST);
             }
         }
     }
